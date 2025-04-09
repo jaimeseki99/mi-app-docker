b0627545188338c95f29fdfe5ce32141a6105d94
jaimeseki99 <jaime99sq@gmail.com>
2025-04-09 19:22:00 +0200

    Modify backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "bucket-git-jaime"               
-    key    = "gitactions/terraform.tfstate"         
+    key    = "terraform.tfstate"         
     region = "us-east-1"             
    }
 }
